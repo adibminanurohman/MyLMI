66a5a8387fbb5ebee181e856b6820585b4cb892b
Muhamad Adib Minanurohman <inanadib084@gmail.com>
2024-03-17 10:14:40 +0700

    add splash screen

---
+++ app/src/main/java/com/laznas/mylmi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.laznas.mylmi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/laznas/mylmi/SplashFragment.kt
@@ -0,0 +1,28 @@
+package com.laznas.mylmi
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.navigate_splashFragment_to_homeFragment)
+        }, 3000)
+        val view = inflater.inflate(R.layout.fragment_splash, container, false)
+        return view
+    }
+}
